dd407636f9decb4be5e63586c333c28f368c11f2
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-01-12 11:26:31 -0600

    Seed reflection questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,54 +12,10 @@
 #   { email: "a_fawziyya@gmail.com", first_name: "Aïcha", last_name: "Fawziyya" } 
 #   ])
 
-# groups = Group.create([
-#   { name: "Roadrunners", user_id: 1 },
-#   { name: "Serpents", user_id: 2 },
-#   { name: "Jaguars", user_id: 3 }
-# ])
-
-group_enrollments = GroupEnrollment.create([
-  { group_id: 1, student_id: 21 },
-  { group_id: 1, student_id: 22 },
-  { group_id: 1, student_id: 23 },
-  { group_id: 1, student_id: 24 },
-  { group_id: 1, student_id: 25 },
-  { group_id: 1, student_id: 26 },
-  { group_id: 1, student_id: 27 },
-  { group_id: 1, student_id: 28 },
-  { group_id: 1, student_id: 29 },
-  { group_id: 1, student_id: 30 },
-  { group_id: 2, student_id: 31 },
-  { group_id: 2, student_id: 32 },
-  { group_id: 2, student_id: 33 },
-  { group_id: 2, student_id: 34 },
-  { group_id: 2, student_id: 35 },
-  { group_id: 3, student_id: 36 },
-  { group_id: 3, student_id: 37 },
-  { group_id: 3, student_id: 38 },
-  { group_id: 3, student_id: 39 },
-  { group_id: 3, student_id: 40 },
+ReflectionQuestion.create([
+  { text: "How do you feel about what you learned today?" },
+  { text: "Was there anything that clicked for you today?" },
+  { text: "Is there anything that is still fuzzy for you?" },
+  { text: "Do you think you could teach this topic to someone else?" },
+  { text: "How confident are you about your understanding of today's topic?" }
 ])
-
-# students = Student.create([
-#   { first_name: "Rúadán", last_name: "Merry" },
-#   { first_name: "Tikhomir", last_name: "Phelix" },
-#   { first_name: "Sargon", last_name: "Hüseyin" },
-#   { first_name: "Boudicca", last_name: "Finnley" },
-#   { first_name: "Miroslava", last_name: "Marie" },
-#   { first_name: "Winona", last_name: "Shou" },
-#   { first_name: "Hari", last_name: "Lachlann" },
-#   { first_name: "Gunner", last_name: "Ulrikke" },
-#   { first_name: "Midas", last_name: "Hector" },
-#   { first_name: "Wulfhram", last_name: "Lidia" },
-#   { first_name: "Queenie", last_name: "Nazaire" },
-#   { first_name: "Asaf", last_name: "Dalal" },
-#   { first_name: "Tanguy", last_name: "Eder" },
-#   { first_name: "Aulikki", last_name: "Livna" },
-#   { first_name: "Alaiya", last_name: "Valéria" },
-#   { first_name: "Andrej", last_name: "Haneef" },
-#   { first_name: "Deepak", last_name: "Baldo" },
-#   { first_name: "Øydis", last_name: "Nino" },
-#   { first_name: "Henrik", last_name: "Ketevan" },
-#   { first_name: "Kristina", last_name: "Mirabell" },
-# ])
