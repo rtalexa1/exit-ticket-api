9e943d373d562c2935dcb8f374400355590cd0fb
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-01-17 10:57:00 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :reflection_questions
-  resources :exit_tickets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users
-  resources :exit_tickets
-  resources :reflection_questions
+  resources :users do
+    resources :exit_tickets
+  end
 end
