68ccff7a451b00486e967d8f6df1eeab94717f76
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-01-18 11:49:25 -0600

    Add image_url column to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_210630) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_174751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_210630) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "exit_ticket_id", null: false
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
