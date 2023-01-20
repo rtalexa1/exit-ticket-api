1e5c2df1efc759c6eb9d45c1a14c20f0abc39048
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-01-20 10:24:05 -0600

    Refactor models

--- app/models/exit_ticket.rb
+++ app/models/exit_ticket.rb
@@ -3,6 +3,10 @@ class ExitTicket < ApplicationRecord
   validates :user_id, :title, :grade_level, :subject_area, presence: true
 
   belongs_to :user
-  has_many :exit_ticket_questions
-
+  has_many :reflection_exit_ticket_questions
+  has_many :reflection_questions, 
+    through: :reflection_exit_ticket_questions
+  has_many :standards_based_exit_ticket_questions
+  has_many :standards_based_questions, 
+    through: :standards_based_exit_ticket_questions
 end
--- app/models/reflection_exit_ticket_question.rb
+++ app/models/reflection_exit_ticket_question.rb
@@ -1,9 +1,7 @@
 class ExitTicketQuestion < ApplicationRecord
 
-  validates :exit_ticket_id, :question_id, presence: true
+  validates :exit_ticket_id, :ref_question_id, presence: true
 
   belongs_to :exit_ticket
   belongs_to :reflection_question
-  belongs_to :standards_based_question
-
 end
