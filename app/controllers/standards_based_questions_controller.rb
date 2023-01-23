38b06cafd0659f3595244dc3551d2146ded2df3d
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-01-22 23:15:09 -0600

    Add methods to standards_based_questions_controller

--- app/controllers/standards_based_questions_controller.rb
+++ app/controllers/standards_based_questions_controller.rb
@@ -1,4 +1,5 @@
 class StandardsBasedQuestionsController < ApplicationController
+  before_action :set_standards_based_question, only: :show
 
   def index
     @standards_based_questions = StandardsBasedQuestion.all
@@ -9,4 +10,15 @@ class StandardsBasedQuestionsController < ApplicationController
   def show
     render json: @standards_based_question
   end
+
+  private
+    def set_standards_based_exit_ticket_question
+      @standards_basedquestion = StandardsBasedQuestion.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def standards_based_question_params
+      params.require(:standards_based_question)
+      .permit(:student_expectation)
+    end
 end
