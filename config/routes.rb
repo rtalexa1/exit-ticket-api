13357457d39f783c9d38c5076787bf46f32534e3
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-02-04 10:37:50 -0600

    Re-nest exit tickets

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users
-  resources :exit_tickets
+  resources :users do
+    resources :exit_tickets
+  end
+
   resources :standards_based_questions, only: [:index, :show]
   resources :standards_based_exit_ticket_questions
   resources :reflection_questions
