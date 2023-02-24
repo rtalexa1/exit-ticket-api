cc0080e834f1cd97b325ec0d67a170f2e287103b
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-02-24 13:29:45 -0600

    Put updated URL in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "exit-ticket-frontend.vercel.app"
+    origins "math-exit-ticket.vercel.app"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
