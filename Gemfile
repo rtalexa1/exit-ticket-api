a00423fbbacd094bc35871d2f38d6f99ff4e9fbd
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-03-02 14:16:40 -0600

    Add should-matchers gem and refactor ExitTicket tests

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem "rspec", "~> 3.12"
   gem "rspec-rails", "~> 6.0.0"
+  gem "shoulda-matchers"
 end
 
 group :development do
--- spec/models/exit_ticket_spec.rb
+++ spec/models/exit_ticket_spec.rb
@@ -1,21 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ExitTicket, type: :model do
-  it "is not valid without a title" do
-    exit_ticket = ExitTicket.new(title: nil)
-
-    expect(exit_ticket).to_not be_valid
-  end
-
-  it "is not valid without a grade level" do
-    exit_ticket = ExitTicket.new(grade_level: nil)
-
-    expect(exit_ticket).to_not be_valid
-  end
-
-  it "is not valid without a subject area" do
-    exit_ticket = ExitTicket.new(subject_area: nil)
-
-    expect(exit_ticket).to_not be_valid
-  end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:grade_level) }
+  it { should validate_presence_of(:subject_area) }
 end
