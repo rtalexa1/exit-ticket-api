ece319ac400c355f18bd292df43aa0e5a80d3322
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-04-14 10:23:45 -0500

    Fix cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://exit-ticket-api.herokuapp.com/"
+    origins "https://math-exit-ticket.vercel.app"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
